6adda0c131a0b660387ae065c2619d39295e5f50
Alec Missine <alec_missine@yahoo.com>
2019-04-26 17:45:54 -0400

    Display the VM and docker info

--- before_install.sh
+++ before_install.sh
@@ -1,11 +1,6 @@
-# Display the VM info
+# Display the VM and docker info
 uname -a
-
-# Display the docker version
-#docker -v
-
-# Display the NodeJS version
-#node -v
+docker info
 
 # Determine the IP address of the VM
 VM_IP_ADDRESS=`ifconfig | grep -A 1 $1 | grep 'inet addr:'`
