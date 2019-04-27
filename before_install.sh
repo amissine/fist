6ab580d37d243262d26ae76b16b255adc1b9a953
Alec Missine <alec_missine@yahoo.com>
2019-04-27 15:47:43 -0400

    Delete before_install.sh

--- before_install.sh
+++
@@ -1,4 +0,0 @@
-# Determine the IP address of the VM
-VM_IP_ADDRESS=`ifconfig | grep -A 1 $1 | grep 'inet addr:'`
-VM_IP_ADDRESS=${VM_IP_ADDRESS:20}
-export VM_IP_ADDRESS=${VM_IP_ADDRESS%% *}
--- script.sh
+++ script.sh
@@ -1,5 +1,15 @@
+# Determine the IP address of the VM
+VM_IP_ADDRESS=`ifconfig | grep -A 1 $1 | grep 'inet addr:'`
+VM_IP_ADDRESS=${VM_IP_ADDRESS:20}
+VM_IP_ADDRESS=${VM_IP_ADDRESS%% *}
 echo "VM_IP_ADDRESS $VM_IP_ADDRESS"
+
+# Start proxy
 sudo -E $(which node) https-proxy.js &
 pid=$!
+
+# Sleep, then kill the proxy
 sleep 5
 sudo kill $pid
+
+echo "$0 exiting"
