3775be7ac5e366de5982704eef70f40c61641b04
Alec Missine <alec_missine@yahoo.com>
2019-04-27 08:43:52 -0400

    Use 'npm test'

--- before_install.sh
+++ before_install.sh
@@ -8,7 +8,3 @@ VM_IP_ADDRESS=`ifconfig | grep -A 1 $1 | grep 'inet addr:'`
 VM_IP_ADDRESS=${VM_IP_ADDRESS:20}
 VM_IP_ADDRESS=${VM_IP_ADDRESS%% *}
 echo $VM_IP_ADDRESS
-
-# Run 'npm install' and launch the proxy
-npm install
-node https-proxy.js &
